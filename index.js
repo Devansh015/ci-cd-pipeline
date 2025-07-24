ad2489129612e4f10bd62f5be757408bdef0f881
Devansh Jain <devansh_jain@outlook.com>
2025-07-23 23:20:17 -0400

    Pipeline boilerplate

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const config = require("dotenv").config;
 
 config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
